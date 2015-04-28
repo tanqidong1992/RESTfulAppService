fe54fec6a85d110cdf68de949a36862f9b242e1b
tanqidong1992 <903843602@qq.com>
2015-04-28 16:25:21 +0800

    增加返回结果实体类

---
+++ src/com/computer/entity/Response.java
@@ -0,0 +1,44 @@
+package com.computer.entity;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSeeAlso;
+
+@XmlRootElement(name="responsde")
+@XmlSeeAlso({User.class,GoodInfo.class,CommentInfo.class})
+public class Response <T>{
+	
+	private Boolean operateResult=true;
+	private T object;
+	private String description;
+	private int errorCode;
+	
+	public Boolean getOperateResult() {
+		return operateResult;
+	}
+	public void setOperateResult(Boolean operateResult) {
+		this.operateResult = operateResult;
+	}
+	
+	public T getObject() {
+		return object;
+	}
+	public void setObject(T object) {
+		this.object = object;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getErrorCode() {
+		return errorCode;
+	}
+	public void setErrorCode(int errorCode) {
+		this.errorCode = errorCode;
+	}
+	
+	
+	
+
+}
